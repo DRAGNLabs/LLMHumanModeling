35e925bcfbcd82522f50d4b490bc41be19ca86bc
Drew Galbraith <drew.s.galbraith@gmail.com>
2023-03-16 18:50:43 -0600

    Tiny bug! Misnamed variable

--- Search Engine/get_wikimedia.py
+++ Search Engine/get_wikimedia.py
@@ -5,7 +5,7 @@ def download_wiki_abstracts():
     with requests.get(url, stream=True) as q:  # See below comment
         q.raise_for_status()
         with open('data/enwiki-latest-abstract.xml.gz', 'wb') as f:
-            for i, chunk in enumerate(r.iter_content(chunk_size=1000000)):  # chunking == 1 MB; repeatedly fetches from the server, thanks to above "stream=True"
+            for i, chunk in enumerate(q.iter_content(chunk_size=1000000)):  # chunking == 1 MB; repeatedly fetches from the server, thanks to above "stream=True"
                 f. write(chunk)
                 if i % 10 == 0:  # Print message every 10 MB?
                     print(f'Downloaded {i} megabytes', end='\r')
