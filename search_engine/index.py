db0bca5df16a502ac7bce169cfe45fbdf1cf93ba
Drew Galbraith <drew.s.galbraith@gmail.com>
2023-04-04 10:52:12 -0600

    Fix r_idx funtion and import errors

--- search_engine/index.py
+++ search_engine/index.py
@@ -1,5 +1,5 @@
 import math
-from .wiki_class import Abstract, get_abs_percent
+from .wiki_class import Abstract
 from .timing import timing
 from . import analysis
 from collections import defaultdict
@@ -74,7 +74,7 @@ class Index:
         """ percent: float [0, 1] -> e.g. .2, .5 """
 
         cap = len(self.documents, percent) 
-        r_idx = (0, cap-1)  # cap-1 bc randint "includes both endpoints."
+        r_idx = (1, cap-1)  # cap-1 bc randint "includes both endpoints."
         abs_2_return = self.documents[r_idx]  # a wiki_class abstract
         curr_perc = self.log[r_idx] % 1
 
