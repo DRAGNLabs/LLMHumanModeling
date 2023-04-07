538d912c069e4f0dc34ea9cb5f4c8ac88db71252
chaz <chaz@rag.net>
2023-04-07 10:24:29 -0600

    Add filepath check in get_wikimedia

--- search_engine/get_wikimedia.py
+++ search_engine/get_wikimedia.py
@@ -1,10 +1,13 @@
 import requests
+import os
 
 def download_wiki_abstracts()-> None:
     url = 'https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-abstract.xml.gz'
     with requests.get(url, stream=True) as q:  # See below comment
         q.raise_for_status()
-        with open('../data/wiki/enwiki-latest-abstract.xml.gz', 'wb') as f:
+        if not os.path.exists("./data/wiki/"):
+            os.mkdir("./data/wiki/")            
+        with open('./data/wiki/enwiki-latest-abstract.xml.gz', 'wb') as f:
             for i, chunk in enumerate(q.iter_content(chunk_size=1000000)):  # chunking == 1 MB; repeatedly fetches from the server, thanks to above "stream=True"
                 f. write(chunk)
                 if i % 10 == 0:  # Print message every 10 MB?
