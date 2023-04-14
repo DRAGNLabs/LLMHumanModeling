56eaff5c240cff5071eec01f99b5e366be65a3a8
Gordon Yu <gordoncyu@gmail.com>
2023-04-14 11:36:01 -0600

    Restructuring and added scrape_wikipedia_article

---
+++ modeling/utils/scrape_wikipedia_article.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_wikipedia_article(url):
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Failed to fetch the page: {response.status_code}")
+        return None
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Extract the article body
+    article_body = ""
+    for paragraph in soup.find_all("p"):
+        article_body += paragraph.text
+
+    # Extract citations
+    citations = []
+    for ref in soup.find_all("span", class_="reference-text"):
+        citations.append(ref.text)
+
+    # Extract extraneous information (info box, tables, etc.)
+    extraneous_info = {}
+    infobox = soup.find("table", class_="infobox")
+    if infobox:
+        extraneous_info["infobox"] = infobox.text
+
+    tables = soup.find_all("table")
+    if tables:
+        extraneous_info["tables"] = []
+        for table in tables:
+            extraneous_info["tables"].append(table.text)
+
+    return {
+        "article_body": article_body,
+        "citations": citations,
+        "extraneous_info": extraneous_info,
+    }
+
+# Example usage:
+# url = "https://en.wikipedia.org/wiki/OpenAI"
+# result = scrape_wikipedia_article(url)
+# print(result)
