5a99212d566a409052075c11d9c84cd5e268253b
Drew Galbraith <drew.s.galbraith@gmail.com>
2023-04-14 16:43:42 -0600

    Integrate function S

--- modeling/birth_agi.py
+++ modeling/birth_agi.py
@@ -2,6 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from utils.scrape_wikipedia_article import scrape_wikipedia_article
 from run_search_engine import create_wiki_index
 from modeling.agentive_functions.stop_function import stop_training as func_f
+from modeling.agentive_functions.data_selector import next_corpus
 from random import random
 import argparse
 
@@ -18,9 +19,8 @@ wiki_index = create_wiki_index()
 did_training: bool = False
 
 while True:
-    article_abs = wiki_index.get_rand_doc()
-    article = scrape_wikipedia_article(wiki_index.get_rand_doc().url)
-    probability_of_stopping = func_f(model, tokenizer, article)
+    txt, abs = next_corpus(wiki_index)
+    probability_of_stopping = func_f(model, tokenizer, txt)
     stop = random() < probability_of_stopping
     if not stop:
         
