8e895d2baafc53261d5af6f9a476c8891dde7794
chaz <chaz@rag.net>
2023-04-14 12:21:15 -0600

    update sear_engine control flow

--- run_search_engine.py
+++ run_search_engine.py
@@ -19,43 +19,43 @@ def index_documents(documents, index: Index)-> Index:
     return index
 
 def do_all():
-    if __name__ == '__main__':
-        # this will only download the xml dump if you don't have a copy already;
-        # just delete the file if you want a fresh copy
-        if not os.path.exists('./data/wiki/enwiki-latest-abstract.xml.gz'):
-            download_wiki_abstracts()
+    # this will only download the xml dump if you don't have a copy already;
+    # just delete the file if you want a fresh copy
+    if not os.path.exists('./data/wiki/enwiki-latest-abstract.xml.gz'):
+        download_wiki_abstracts()
 
-        search_index: Index  # Declare var for 'scoping()'
-        with scoping(): # Loads indexes and documents from cache if they exist, otherwise creates them
-            if os.path.exists('./search_engine/cache/index.json') and os.path.exists('./search_engine/cache/documents.json'):  # Load existing index
-                raw_indexes: dict = json.load(open('./search_engine/cache/index.json'))
-                raw_documents: list = json.load(open('./search_engine/cache/documents.json'))
-                raw_documents = [Abstract(ID, title, abtract, url) for ID, title, abtract, url in raw_indexes]
-                search_index = Index(raw_documents, raw_indexes)
-            else:  # Create index
-                search_index = index_documents(load_documents(), Index())
-            
-            scoping.keep('search_index') # passes search_index to the enclosing scope (otherwise the variable would refer to the uninitialized Index() object)
+    search_index: Index  # Declare var for 'scoping()' funtion
+    with scoping(): # Loads indexes and documents from cache if they exist, otherwise creates them
+        if os.path.exists('./search_engine/cache/index.json') and os.path.exists('./search_engine/cache/documents.json'):  # Load existing index
+            raw_indexes: dict = json.load(open('./search_engine/cache/index.json'))
+            raw_documents: list = json.load(open('./search_engine/cache/documents.json'))
+            raw_documents = [Abstract(ID, title, abtract, url) for ID, title, abtract, url in raw_indexes]
+            search_index = Index(raw_documents, raw_indexes)
+        else:  # Create index
+            search_index = index_documents(load_documents(), Index())
 
         print(f'Index contains {len(search_index.documents)} documents.')
-        # print(search_index.index)
-        # print(search_index.documents)
+        scoping.keep('search_index') # passes search_index to the enclosing scope; otherwise the variable would refer to the uninitialized Index() object.
 
+    if not os.path.exists('./search_engine/cache/search_index.json') or not os.path.exists('./search_engine/cache/documents.json'):
+        
         if not os.path.exists('./search_engine/cache'):  # Make a 'cache' directory
             os.mkdir('./search_engine/cache')
 
-            fl_vs: str = ""  # str to denote model run being logged.
-            with open(f'./search_engine/cache/search_index_{fl_vs}.json', 'w') as f:
-                json_serializable_index = {index_str: list(set_ints) for index_str, set_ints in search_index.index.items()}
-                json.dump(json_serializable_index, f, indent=4)
-            with open(f'./search_engine/cache/documents_{fl_vs}.json', 'w') as f:
-                json.dump([[doc.ID, doc.title, doc.abstract, doc.url] for doc in search_index.documents.values()], f, indent=4)
-        abs_2_search = Index.get_rand_doc()
-        
-        return abs_2_search
+        # Store search_index as .json 
+        with open(f'./search_engine/cache/search_index.json', 'w') as f:
+            json_serializable_index = {index_str: list(set_ints) for index_str, set_ints in search_index.index.items()}
+            json.dump(json_serializable_index, f, indent=4)
+        # Store documents as .json 
+        with open(f'./search_engine/cache/documents.json', 'w') as f:
+            json.dump([[doc.ID, doc.title, doc.abstract, doc.url] for doc in search_index.documents.values()], f, indent=4)
+    
+    abs_2_search = Index.get_rand_doc()
+    
+    return abs_2_search
 
-    ## Example searches
-        search_index.search('London Beer Flood', search_type='AND')
-        # search_index.search('London Beer Flood', search_type='OR')
-        # search_index.search('London Beer Flood', search_type='AND', rank=True)
-        # search_index.search('London Beer Flood', search_type='OR', rank=True)
+## Example searches
+    search_index.search('London Beer Flood', search_type='AND')
+    # search_index.search('London Beer Flood', search_type='OR')
+    # search_index.search('London Beer Flood', search_type='AND', rank=True)
+    # search_index.search('London Beer Flood', search_type='OR', rank=True)
