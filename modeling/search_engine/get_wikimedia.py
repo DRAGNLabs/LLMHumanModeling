97ccfc3a498e49192c61a55c69f4ff00a53fd024
Drew Galbraith <drew.s.galbraith@gmail.com>
2023-04-18 14:43:49 -0600

    Wait, no, this one fixes the parentheses.

--- modeling/search_engine/get_wikimedia.py
+++ modeling/search_engine/get_wikimedia.py
@@ -11,7 +11,7 @@ def download_wiki_abstracts()-> None:
         wiki_dir_path = os.path.join(PERSISTENT_DIR, 'data/wiki/')
         if not os.path.exists(wiki_dir_path):
             os.mkdir(wiki_dir_path)            
-        with open(os.path.join(wiki_dir_path, 'enwiki-latest-abstract.xml.gz', 'wb')) as f:
+        with open(os.path.join(wiki_dir_path, 'enwiki-latest-abstract.xml.gz'), mode = 'wb') as f:
             for i, chunk in enumerate(q.iter_content(chunk_size=1000000)):  # chunking == 1 MB; repeatedly fetches from the server, thanks to above "stream=True"
                 f. write(chunk)
                 if i % 10 == 0:  # Print message every 10 MB?
