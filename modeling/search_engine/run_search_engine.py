0c812f29ab0df3bbf19b76c0878d33084944d252
Drew Galbraith <drew.s.galbraith@gmail.com>
2023-04-18 17:20:59 -0600

    Enable use of truncated data

--- modeling/search_engine/run_search_engine.py
+++ modeling/search_engine/run_search_engine.py
@@ -23,40 +23,42 @@ def index_documents(documents, index: Index)-> Index:
             print(f'Indexed {i} documents', end='\r')
     return index
 
-def create_wiki_index():
+def create_wiki_index(truncate=True):
     # this will only download the xml dump if you don't have a copy already;
     # just delete the file if you want a fresh copy
-    wiki_data_path = os.path.join(PERSISTENT_DIR, 'data/wiki/enwiki-latest-abstract.xml.gz')
+    trunc = "truncated_" if truncate else ""
+        
+    wiki_data_path = os.path.join(PERSISTENT_DIR, f'data/wiki/{trunc}enwiki-latest-abstract.xml.gz')
     if not os.path.exists(wiki_data_path):
         print(os.path.abspath(wiki_data_path), os.getcwd())
         download_wiki_abstracts()
 
     search_index: Index  # Declare var for 'scoping()' funtion
     with scoping(): # Loads indexes and documents from cache if they exist, otherwise creates them
-        print(f"\nChecking index.json exists: {os.path.exists(join_module_path('cache/index.json'))}")
-        print(f"Checking documents.json exists: {os.path.exists(join_module_path('cache/documents.json'))}\n")
-        if os.path.exists(join_module_path('cache/index.json')) and os.path.exists(join_module_path('cache/documents.json')):  # Load existing index
-            raw_indexes: dict = json.load(open(join_module_path('cache/index.json')))
-            raw_documents: list = json.load(open(join_module_path('cache/documents.json')))
+        print(f"\nChecking index.json exists: {os.path.exists(join_module_path('cache/{trunc}index.json'))}")
+        print(f"Checking documents.json exists: {os.path.exists(join_module_path('cache/{trunc}documents.json'))}\n")
+        if os.path.exists(join_module_path('cache/{trunc}index.json')) and os.path.exists(join_module_path('cache/{trunc}documents.json')):  # Load existing index
+            raw_indexes: dict = json.load(open(join_module_path('cache/{trunc}index.json')))
+            raw_documents: list = json.load(open(join_module_path('{trunc}cache/documents.json')))
             raw_documents = [Abstract(ID, title, abtract, url) for ID, title, abtract, url in raw_documents]
             search_index = Index(raw_documents, raw_indexes)
         else:  # Create index
-            search_index = index_documents(load_documents(), Index())
+            search_index = index_documents(load_documents(truncate=truncate), Index())
 
         print(f'Index contains {len(search_index.documents)} documents.')
         scoping.keep('search_index') # passes search_index to the enclosing scope; otherwise the variable would refer to the uninitialized Index() object.
 
-    if not os.path.exists(join_module_path('cache/index.json')) or not os.path.exists(join_module_path('cache/documents.json')):
+    if not os.path.exists(join_module_path('cache/{trunc}index.json')) or not os.path.exists(join_module_path('cache/{trunc}documents.json')):
         
         if not os.path.exists(join_module_path('cache')):  # Make a 'cache' directory
             os.mkdir(join_module_path('cache'))
 
         # Store search_index as .json 
-        with open(join_module_path(f'cache/index.json'), 'w') as f:
+        with open(join_module_path(f'cache/{trunc}index.json'), 'w') as f:
             json_serializable_index = {index_str: list(set_ints) for index_str, set_ints in search_index.index.items()}
             json.dump(json_serializable_index, f, indent=4)
         # Store documents as .json 
-        with open(join_module_path(f'cache/documents.json'), 'w') as f:
+        with open(join_module_path(f'cache/{trunc}documents.json'), 'w') as f:
             json.dump([[doc.ID, doc.title, doc.abstract, doc.url] for doc in search_index.documents.values()], f, indent=4)
     
     return search_index
