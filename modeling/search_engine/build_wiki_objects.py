12c8cbeed55db1371867ca70fe09fa24d6f7a026
Gordon Yu <gordoncyu@gmail.com>
2023-04-18 12:13:13 -0600

    Fixings paths

--- modeling/search_engine/build_wiki_objects.py
+++ modeling/search_engine/build_wiki_objects.py
@@ -1,13 +1,17 @@
 import gzip
 from lxml import etree  # Can't find this package; pip install lxml?
 from time import time
+import os
 
 from . import wiki_class  # local .py file
+
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+PERSISTENT_DIR = os.path.join(MODULE_DIR, '../../')
 
 def load_documents()-> wiki_class.Abstract:
     start = time()
     # open a filehandle to the gzipped Wikipedia dump
-    with gzip.open('./data/wiki/enwiki-latest-abstract.xml.gz', 'rb') as f:
+    with gzip.open(os.path.join(PERSISTENT_DIR, './data/wiki/enwiki-latest-abstract.xml.gz'), 'rb') as f:
         doc_id = 1
         # iterparse will yield the entire `doc` element once it finds the
         # closing `</doc>` tag
