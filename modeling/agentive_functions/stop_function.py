23b52a63162f4ba7eb64befe1ab77e8e209730d5
Drew Galbraith <drew.s.galbraith@gmail.com>
2023-04-20 18:41:37 -0600

    Add clarfying comments

--- modeling/agentive_functions/stop_function.py
+++ modeling/agentive_functions/stop_function.py
@@ -2,13 +2,16 @@ import math
 from .llm_functions.perplexity import get_ppl
 
 def sigmoid(x):
+    """A sigmoid-esque function created through roughly gauging WolfRam Alpha visualizations"""
     return 1 / (
         1 + math.exp(
             -((x-5)/1.8)
             )
         )
 
-def stop_training(model, tokenizer = None, text: str = None, tokens = None, ideal_ppl: float = 25, device = None) -> float:
+def stop_or_continue(model, tokenizer = None, text: str = None, tokens = None, ideal_ppl: float = 25, device = None) -> float:
+    """Calculate probability of stopping training on a given text."""
+    
     # Calculate the perplexity of the model on the given text
     model_ppl = get_ppl(model, tokenizer, text, device=device)
 
